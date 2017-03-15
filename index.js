e39c41662be5d5e77272266deecdd88c1246cfcc
mackenza <a.mackenzie@gmail.com>
2017-03-14 23:36:45 -0400

    working on modules

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const atable = require('ascii-table');
-const scraper = require('osmosis');
+require('dotenv').config();
 const Discord = require('discord.js');
+const parseCommands = require('./lib/commands/all').parseCommands();
 
 // create an instance of a Discord Client, and call it bot
 const bot = new Discord.Client();
 
 // the token of your bot - https://discordapp.com/developers/applications/me
-const token = 'MjkwNjQwODE4ODI4NjA3NDg5.C6ixKQ.3r2aWhXy2twjtxNPOytx-FzHvYQ';
+const token = process.env.TOKEN;
 
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
@@ -16,36 +16,9 @@ bot.on('ready', () => {
 
 // create an event listener for messages
 bot.on('message', message => {
-  if (message.content === '!mlb avg') {
-    let avgData = getAvg(message);
-  }
+  message.channel.sendMessage(parseCommands(message.content));
 });
 
 // log our bot in
 bot.login(token);
 
-function getAvg(message) {
-    let avgs = new atable('Top 10 by batting avg');
-    avgs.setHeading('Rank', 'POS', 'Player', 'AVG');
-    scraper
-    .get('http://theshownation.com/franchises/275472/leaders/AVG')
-    .find('table.table tbody tr')
-    .set({
-        'team': 'td[1]',
-        'rank': 'td[2]',
-        'pos': 'td[3]',
-        'player': 'td[4]',
-        'avg': 'td[5]'
-    })
-    .data(function(found) {
-        avgs.addRow(found.rank, found.pos, found.player, found.avg);
-    })
-    .done(function(thedata) {
-        //avgs.removeBorder();
-        console.log(avgs.toString());
-        message.channel.sendMessage('``` \n' + avgs.toString() + '\n ```');
-    })
-    .log(console.log)
-    .error(console.log)
-    .debug(console.log)
-}
---
+++ test.js
@@ -0,0 +1,5 @@
+const parseMessage = require('./lib/commands/all');
+
+var messageText = "!mlb avg";
+
+parseMessage.parseMessage(messageText);
